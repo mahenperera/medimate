1cbce7b26e98bfb2330f019b750b78ea665ae48c
Mahen Perera <kmaperera864@gmail.com>
2026-01-25 21:48:56 +0530

    First commit.

---
+++ backend/src/main/java/com/medimate/MediMateApplication.java
@@ -0,0 +1,13 @@
+package com.medimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/medimate/MediMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
